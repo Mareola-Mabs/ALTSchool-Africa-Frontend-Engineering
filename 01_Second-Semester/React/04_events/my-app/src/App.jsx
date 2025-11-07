f7d5ce685dfcfe25c493fb84a03deb6a17dbf3c4
Mareola-Mabs <bulun1001@gmail.com>
2025-11-07 11:59:18 +0100

    feat: add events in React

---
+++ 01_Second-Semester/React/04_events/my-app/src/App.jsx
@@ -0,0 +1,19 @@
+import Button from './components/Button'
+import Counter from './components/Counter'
+
+
+// Create root Component
+const App = () =>{
+  return (
+    <div>
+      <Counter/>
+      <Button/>
+    </div>
+  )
+}
+
+
+
+
+
+export default App
---
+++ 01_Second-Semester/React/04_events/my-app/src/components/Button.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+
+// Handle click event
+// const handleClick = (event)=>{
+//     console.log("Hello, you clicked me", "The event type is:", event.type)
+
+// }
+
+
+
+export default function Button(){
+    const [counter, setCounter] = useState(0)
+
+
+    const increment = ()=>{
+        setCounter(prev => prev + 1)
+    }
+    const decrement = ()=>{
+        setCounter(prev => prev - 1)
+    }
+
+    return <div>
+        <div>{counter}</div>
+        <button onClick={increment} className="increment">Increment</button>
+        <button onClick={decrement} className="decrement">Decrement</button>
+    </div>
+}
