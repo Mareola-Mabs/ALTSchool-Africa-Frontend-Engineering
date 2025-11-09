cf0a22c71cbc5936620793322c08413d07a56a32
Mareola-Mabs <bulun1001@gmail.com>
2025-11-10 00:56:01 +0100

    feat: add form events

--- 01_Second-Semester/React/07_change-events/my-app/src/App.jsx
+++ 01_Second-Semester/React/07_change-events/my-app/src/App.jsx
@@ -1,22 +1,44 @@
 import "./assets/css/style.css";
 
+function handleChange(event) {
+  event.preventDefault();
+  console.log("value: ", event.target.value);
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const { name, email, tel } = e.target.elements
+
+  console.log(name.value, email.value, tel.value)
+}
+
 // Create root Component
 const App = () => {
-  function handleChange(event) {
-    event.preventDefault();
-    console.log("value: ", event.target.value);
-  }
-
   return (
     <section className="form__container">
-      <form action="" className="form">
+      <form action="" className="form" onSubmit={handleSubmit}>
         <input
+          className="name"
           type="text"
           placeholder="Enter your name"
           onChange={handleChange}
+          name="name"
+          required
         />
-        <input type="text" placeholder="Enter your email" />
-        <input type="text" placeholder="Enter your phone" />
+        <input
+          type="email"
+          className="email"
+          placeholder="Enter your email"
+          name="email"
+          required
+        />
+        <input
+          type="number"
+          className="phone"
+          placeholder="Enter your phone"
+          name="tel"
+          required
+        />
         <input type="submit" value="Submit" />
       </form>
     </section>
