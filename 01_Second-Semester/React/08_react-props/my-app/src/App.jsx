3d0e094b4a57e94c04a5ea30a6a2136d505c27b5
Mareola-Mabs <bulun1001@gmail.com>
2025-11-10 13:30:12 +0100

    feat: add react props

---
+++ 01_Second-Semester/React/08_react-props/my-app/src/App.jsx
@@ -0,0 +1,61 @@
+// Car Component
+const Car = (props) => {
+  console.log(props); // {data: {brand: "Tesla", id: 1}, brand: '1x625', price: "$1", manufactureYear: "2025"}
+  console.log(props.model); // 1x625
+
+  const {data: {id, brand, year, color}, model, manufactureYear} = props // Destructuring the props object
+  console.log(id, color, manufactureYear) // 1, Red, 2025
+
+  return <div className="car">{`${props.data.brand} ${props.model}`}</div>;
+};
+
+// Create root Component
+const App = () => {
+  const data = {
+    price: "$1",
+    manufactureYear: "2025"
+  } // data to be used as props
+
+
+  const carData = [
+    {
+      brand: 'Ford',
+      color: 'red',
+      year: "2020"
+    },
+    {
+      brand: 'Mercedes',
+      color: 'black',
+      year: "2025"
+    },
+    {
+      brand: 'Lexus',
+      color: 'black',
+      year: "2025"
+    }
+  ]
+
+  const ListItems = ()=>{
+    return carData.map(item =>{
+      return <li key={item.brand} year={item.year}>{item.brand}</li>
+    })
+  }
+
+  return (
+    <div>
+      <Car data={{  
+        id: 1,
+        brand: "Tesla",
+        year: 2020,
+        color: "Red"
+      }} 
+        model="1x625" {...data} />
+
+        <ul>
+          <ListItems/>
+        </ul>
+    </div>
+  );
+};
+
+export default App;
