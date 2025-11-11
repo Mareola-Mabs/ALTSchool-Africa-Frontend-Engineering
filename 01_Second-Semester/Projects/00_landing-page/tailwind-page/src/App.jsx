c58250f430af5f9b96b1eac2b7dc16241b8d9be4
Mareola-Mabs <bulun1001@gmail.com>
2025-11-11 10:23:28 +0100

    chore: init project with react and tailwindcss

---
+++ 01_Second-Semester/Projects/00_landing-page/tailwind-page/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+
+// Root Component
+const App = () => {
+
+  return (
+      <div className='text-3xl font-bold underline bg-red-500'>
+        Hello World
+      </div>
+
+  )
+}
+
+export default App
