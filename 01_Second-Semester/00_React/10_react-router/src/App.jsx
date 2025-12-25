d5e0bbbeb887f08ccd4e6a4200438d43005e1e83
Mareola-Mabs <bulun1001@gmail.com>
2025-12-25 12:20:52 +0100

    feat: add react router

--- 01_Second-Semester/00_React/10_react-router/src/App.jsx
+++ 01_Second-Semester/00_React/10_react-router/src/App.jsx
@@ -1,8 +1,40 @@
+// Import Route and Routes
+import {Routes, Route, Link, NavLink} from 'react-router-dom'
+import './assets/app.css'
+
+
+// Create a Home Page Component
+const Home = ()=>{
+  return(
+    <section className="home__container">
+      <h1>Home Page</h1>
+      <a href="/about" className='navigate'>About</a> {/* This will cause a page reload*/}
+    </section>
+  )
+}
+
+// Create About Page Component
+const About = ()=>{
+  return(
+    <section className="about__container">
+      <h1>About Page</h1>
+      <Link className='navigate' to="/">Home</Link> {/* This will not cause a page reload */}
+      <NavLink style={({isActive})=> isActive? {color: 'red'}: {color: 'blue'}} className='navigate' to="/">Home</NavLink> {/* This will not cause a page reload */}
+      <NavLink style={({isActive})=> isActive? {color: 'red'}: {color: 'blue'}} className='navigate' to="/about">About</NavLink> {/* This will not cause a page reload */}
+    </section>
+  )
+}
+
 
 // Root Component
 const App = ()=>{
   return(
-    <>Hello</>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </>
   )
 }
 
--- 01_Second-Semester/00_React/10_react-router/src/main.jsx
+++ 01_Second-Semester/00_React/10_react-router/src/main.jsx
@@ -1,12 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
+// Import Browser Router
+import { BrowserRouter } from "react-router-dom";
 
 // Import Root Component
-import App from './App.jsx'
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+createRoot(document.getElementById("root")).render(
+  // Wrap application in BrowserRouter
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
+);
