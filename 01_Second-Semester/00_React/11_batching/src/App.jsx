7159e42ee77ea77b7add5f8c70122dac97f4425b
Mareola-Mabs <bulun1001@gmail.com>
2026-01-01 12:15:19 +0100

    feat: add batching in promises, async calls and timeouts

--- 01_Second-Semester/00_React/11_batching/src/App.jsx
+++ 01_Second-Semester/00_React/11_batching/src/App.jsx
@@ -8,6 +8,9 @@ import './assets/app.css'
 const Home = ()=>{
   const [state, setState] = useState(0)
 
+  const [pending, setPending] = useState(0)
+  const [completed, setCompleted] = useState(0)
+
   function changeState(){
 
     // Here, React Batches all state, and render them as one, so "state" only increases by one instead of three
@@ -21,11 +24,35 @@ const Home = ()=>{
     setState(state=> state+1) // No Batching when code runs
   }
 
+
+  function timeout(delay){
+    return new Promise((res, rej)=>{
+      setTimeout(()=>{
+      console.log("Hello")
+      res()
+    }, delay)
+    })
+  }
+
+  async function buy(){
+    setPending(pending => pending + 1)
+
+    await timeout(3000)
+
+    setPending(pending => pending - 1)
+    setCompleted(completed => completed + 1)
+  }
+
   return(
     <section className="home__container">
       <button onClick={changeState}>Click Me</button>
 
+      <button onClick={buy}>Buy</button>
+
       <div>This is the state: {state}</div>
+
+      <div>Pending: {pending}</div>
+      <div>Completed: {completed}</div>
     </section>
   )
 }
