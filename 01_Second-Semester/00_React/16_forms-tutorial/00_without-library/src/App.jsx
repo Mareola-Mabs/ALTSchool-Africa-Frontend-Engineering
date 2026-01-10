a3bc51999fa8fe40a603df03d5d344b12c37db2c
Mareola-Mabs <bulun1001@gmail.com>
2026-01-10 13:50:30 +0100

    feat: handle react form with uncontrolled components

--- 01_Second-Semester/00_React/16_forms-tutorial/00_without-library/src/App.jsx
+++ 01_Second-Semester/00_React/16_forms-tutorial/00_without-library/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import "./assets/app.css";
 
+import { useRef } from "react";
+
 // Form Controls
 
 // Root Component
 function App() {
   // Value States
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [jobRole, setJobRole] = useState("");
+  // const [userName, setUserName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [jobRole, setJobRole] = useState("");
+
+  const usernameRef = useRef();
+  const emailRef = useRef();
+  const jobroleRef = useRef();
 
   // Error States
   const [userNameError, setUserNameError] = useState("");
@@ -21,7 +27,7 @@ function App() {
 
     let isUsernameValid = true;
 
-    if (userName === "") {
+    if (username === "") {
       setUserNameError("Username cannot be empty");
       isUsernameValid = false;
       return isUsernameValid;
@@ -70,62 +76,97 @@ function App() {
       setJobRoleError("Kindly choose a job role");
     }
 
-    return isJobRoleValid
+    return isJobRoleValid;
   }
 
   // OnSubmit Function
   function handleSubmit(e) {
     e.preventDefault();
 
-    let isUsernameValid = valiDateUsername(userName);
-    let isEmailValid = validateEmail(email);
-    let isJobRoleValid = validateJobRole(jobRole);
+    /* For Controlled Components */
+    // let isUsernameValid = valiDateUsername(userName);
+    // let isEmailValid = validateEmail(email);
+    // let isJobRoleValid = validateJobRole(jobRole);
+
+    /* For Uncontrolled Components */
+    let isUsernameValid = valiDateUsername(usernameRef.current.value);
+    let isEmailValid = validateEmail(emailRef.current.value);
+    let isJobRoleValid = validateJobRole(jobroleRef.current.value);
+
 
     if (isUsernameValid && isEmailValid && isJobRoleValid) {
-      console.log(`userName: ${userName}`);
-      console.log(`email: ${email}`);
-      console.log(`jobRole: ${jobRole}`);
+      console.log(`userName: ${usernameRef.current.value}`);
+      console.log(`email: ${emailRef.current.value}`);
+      console.log(`jobRole: ${jobroleRef.current.value}`);
     } else {
       alert("Form data is Invalid");
     }
   }
 
   return (
+    /* For Controlled Components */
+
+    // <form onSubmit={handleSubmit} noValidate>
+    //   <div className="form-group">
+    //     <label htmlFor="username">Username:</label>
+    //     <input
+    //       type="text"
+    //       name=""
+    //       id="username"
+    //       onChange={(e) => setUserName(e.target.value)}
+    //     />
+    //     {userNameError && <p className='error'>{userNameError}</p>}
+    //   </div>
+
+    //   <div className="form-group">
+    //     <label htmlFor="email">Email:</label>
+    //     <input
+    //       type="email"
+    //       name=""
+    //       id="email"
+    //       onChange={(e) => setEmail(e.target.value)}
+    //     />
+    //     {emailError && <p className='error'>{emailError}</p>}
+    //   </div>
+
+    //   <div className="form-group">
+    //     <label htmlFor="jobrole">Email:</label>
+    //     <select
+    //       name=""
+    //       id="jobrole"
+    //       onChange={(e) => setJobRole(e.target.value)}
+    //     >
+    //       <option value="">Select a job role</option>
+    //       <option value="developer">Developer</option>
+    //       <option value="tester">Tester</option>
+    //     </select>
+    //     {jobRoleError && <p className='error'>{jobRoleError}</p>}
+    //   </div>
+    //   <button type="submit">Submit</button>
+    // </form>
+
+    /* For Uncontrolled Components */
     <form onSubmit={handleSubmit} noValidate>
       <div className="form-group">
         <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          name=""
-          id="username"
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        {userNameError && <p className='error'>{userNameError}</p>}
+        <input type="text" name="" id="username" ref={usernameRef} />
+        {userNameError && <p className="error">{userNameError}</p>}
       </div>
 
       <div className="form-group">
         <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          name=""
-          id="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        {emailError && <p className='error'>{emailError}</p>}
+        <input type="email" name="" id="email" ref={emailRef} />
+        {emailError && <p className="error">{emailError}</p>}
       </div>
 
       <div className="form-group">
         <label htmlFor="jobrole">Email:</label>
-        <select
-          name=""
-          id="jobrole"
-          onChange={(e) => setJobRole(e.target.value)}
-        >
+        <select name="" id="jobrole" ref={jobroleRef}>
           <option value="">Select a job role</option>
           <option value="developer">Developer</option>
           <option value="tester">Tester</option>
         </select>
-        {jobRoleError && <p className='error'>{jobRoleError}</p>}
+        {jobRoleError && <p className="error">{jobRoleError}</p>}
       </div>
       <button type="submit">Submit</button>
     </form>
